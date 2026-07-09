5a16d3e7b189e8d30f5aa760aa24920e792cb3a3
LizzyonGit <lizzyvanrhijn@hotmail.com>
2026-07-09 18:04:34 +0200

    link in nav

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,25 @@
 import Link from "next/link";
 
+const links = [
+    {href: "/", label: "Home"},
+    {href: "/about", label: "About"},
+    {href: "/contact", label:"Contact"},
+
+]
+// links.map((link)=> ( "link" är namnet på variabeln som används i map-funktionen, och "link" är också namnet på objektet i arrayen "links". Det är en vanlig praxis att använda samma namn för variabeln som representerar varje objekt i arrayen när man använder map-funktionen. Detta gör
 export default function Navbar() {
   return (
     <nav className="flex space-between gap-4 p-4">
-      <Link href="/">Home</Link>
+        {links.map((link)=> (
+            <Link 
+            key={link.href} 
+            href={link.href}
+            >
+                {link.label}
+            </Link>))}
+      {/* <Link href="/">Home</Link>
       <Link href="/about">About</Link>
-      <Link href="/contact">Contact</Link>
+      <Link href="/contact">Contact</Link> */}
     </nav>
   );
 }
